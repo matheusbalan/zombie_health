aa8ff5fc0a74b9c7585f4c275ed77846ab819ffc
matheusbalan <matheus.balan.sobreira.15@gmail.com>
2019-04-21 20:14:55 -0300

    Doctor and Patient classes

---
+++ ZombieHealth/src/zombies/Doctor.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zombies;
+
+import interfaces.IDoctor;
+import interfaces.IResponder;
+
+/**
+ *
+ * @author Matheus
+ */
+public class Doctor implements IDoctor {
+
+    @Override
+    public void startInterview() {
+        /*TODO*/
+    }
+
+    @Override
+    public void connect(IResponder responder) {
+        /*TODO*/
+    }
+    
+}
---
+++ ZombieHealth/src/zombies/Patient.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zombies;
+
+import interfaces.IPatient;
+
+/**
+ *
+ * @author Matheus
+ */
+public class Patient implements IPatient {
+
+    @Override
+    public String ask(String question) {
+        /*TODO*/
+        return "";
+    }
+
+    @Override
+    public boolean finalAnswer(String answer) {
+        /*TODO*/
+        return true;
+    }
+    
+}
