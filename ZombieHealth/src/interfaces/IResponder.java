bd131ab4def0f5b55a5f60561053dd70761351e8
matheusbalan <matheus.balan.sobreira.15@gmail.com>
2019-04-21 19:41:07 -0300

    Project + interfaces

---
+++ ZombieHealth/src/interfaces/IResponder.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfaces;
+
+/**
+ *
+ * @author Matheus
+ */
+public interface IResponder {
+    public String ask(String question);
+    public boolean finalAnswer(String answer);
+}
