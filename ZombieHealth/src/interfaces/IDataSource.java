62940e06f2fbfbd8cba73b7ab956c650d136cc0a
matheusbalan <matheus.balan.sobreira.15@gmail.com>
2019-04-21 21:19:49 -0300

    New interfaces added

---
+++ ZombieHealth/src/interfaces/IDataSource.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfaces;
+
+/**
+ *
+ * @author Matheus
+ */
+public interface IDataSource {
+    public String getDataSource();
+    public void setDataSource(String dataSource);
+}
